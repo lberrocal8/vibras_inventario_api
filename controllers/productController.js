f8b2f159fd7c48afac634094390136eed2434d76
Luis Carlos Berrocal <69999756+lberrocal8@users.noreply.github.com>
2025-05-16 15:41:29 -0500

    v1.0.1

--- controllers/productController.js
+++ controllers/productController.js
@@ -1,19 +1,25 @@
 const getProduct = (req, res) => {
-  res.json({ message: 'Obteniendo todos los productos...' });
+  res.json({ message: 'Obteniendo todos los productos...', });
 }
 
 const getProductById = (req, res) => {
   const productId = req.params.id;
-  res.json({ message: `Obteniendo el producto con id ${productId}` });
+  res.json({ message: `Obteniendo el producto con id ${productId}`, });
 }
 
 const createProduct = (req, res) => {
   const newProduct = req.body;
   res.json({
-    message: 'Product created!',
+    message: 'Producto creado con exito!',
     data: {
       barCode: `${newProduct.barCode}`,
-      productName: `${newProduct.productName}`,
+      nombre_prenda: `${newProduct.nombre_prenda}`,
+      tipo_tela: `${newProduct.tipo_tela}`,
+      tallas_disponibles: `${newProduct.tallas_disponibles}`,
+      colores_disponibles: `${newProduct.colores_disponibles}`,
+      cantidad_entrante: `${newProduct.cantidad_entrante}`,
+      genero_objetivo: `${newProduct.genero_objetivo}`,
+      estado_prenda: `${newProduct.estado_prenda}`,
     },
   })
 }
--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const productRoutes = require('./routes/productRoutes');
+const cors = require('cors');
 
 const app = express();
-const port = 3000;
+const port = 3010;
 
+app.use(cors());
 app.use(express.json());
 app.use('/api', productRoutes);
 
